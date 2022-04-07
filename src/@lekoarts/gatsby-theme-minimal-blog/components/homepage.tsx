0bc1f8df6301ab7203d4ee7dd24963af95d72ddd
Kenneth Larsen <mail@kennethlarsen.org>
2022-04-07 13:28:25 +0200

    Update homepage.tsx

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -1,20 +1,15 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { Link } from "gatsby"
-import Title from "./title"
-import replaceSlashes from "../utils/replaceSlashes"
-// @ts-ignore
 import Hero from "../texts/hero"
 // @ts-ignore
 import Bottom from "../texts/bottom"
 
 
 const Homepage = () => {
-  const { siteTitle } = useSiteMetadata()
 
   return (
     <Layout>
-      <h1 sx={visuallyHidden}>{siteTitle}</h1>
+      <h1 sx="Kenneth Larsen"</h1>
       <section sx={{ mb: [5, 6, 7], p: { fontSize: [1, 2, 3], mt: 2 }, variant: `section_hero` }}>
         <Hero />
       </section>
